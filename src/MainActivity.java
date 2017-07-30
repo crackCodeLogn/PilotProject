bf571c6da3ff9d6b1ad1bbdd67ebf4201b735a4f
crackCodeLogn <vivektheintel@gmail.com>
2017-07-30 22:01:34 +0530

    Initial commit

---
+++ src/MainActivity.java
@@ -0,0 +1,10 @@
+/**
+ * @author Vivek
+ * @version 1.0
+ * @since 30-07-2017
+ */
+public class MainActivity {
+    public static void main(String[] args) {
+        System.out.println("Marking the start of something new and interesting... Let's see how this goes and how far it trickles");
+    }
+}
